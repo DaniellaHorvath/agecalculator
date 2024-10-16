54321fcba0075b6033080c03eaa2f542e8401804
DaniellaHorvath <142007967+DaniellaHorvath@users.noreply.github.com>
2024-10-16 11:18:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // # dogAgeCalculator
 
-// ## Activity Directions
-// You know how old your dog is in human years, but what about dog years? Calculate it!
-
-// Write a function named calculateDogAge that:
-
-// 1. Takes 1 argument: your dog’s age.
-// 2. Calculates your dog’s age based on the conversion rate of:
-// - 1 human year to 7 dog years.
-// 3. Return a string describing your dog’s age (see below).
-// 4. Run your code.
-
 function calculateDogAge(age){
     const dogAgeCalculator = age * 7;
     return `Your dog age is ${dogAgeCalculator} in human years.`;
@@ -27,24 +16,7 @@ console.log(calculateDogAge(9));
 
 
 // ## Activity
-// Do you know how old your cat is in human years, but what about dog years? Calculate it!
-
-// Write a function named calculateCatAge that:
-
-// 1. Takes 1 argument: your cat’s age.
-// 2. Calculates your cat’s age based on the conversion rate of:
-//The first year of a cat's life is equal to 15 human years, while the second year 
-// is equal to an additional nine years. After the second year of a cat's life, 
-// each additional year is equal to about four human years
-// - 1 human year to 7 dog years.
-// - 1 human year to 15 cat years.
-// - 2 human year increased by 9 cat years.
-// - 3 human years increased by 4 cat years.
-// - 4 human years increased by 4 cat years and so on ...
-// Molly cat is 10 years old in 
-// 3. Return a Number (Optional: string describing your dog’s age (see below).)
-// 4. Run your code.
-// 6. Test your code.
+// # catAgeCalculator
 
 const calcCatAge = (catAge) => {
     if(catAge === 1){
@@ -60,17 +32,8 @@ const calcCatAge = (catAge) => {
 const mollyCatAge = calcCatAge(23); 
 console.log(mollyCatAge); // returns 108
 
-
+// ## Activity
 // # yardsToInches
-
-// ## Activity Directions
-// 1. Declare a function named yards2Inches that will take a number of yards as input.
-// 2. Your function is to do the following:
-// - Convert the yards to inches
-// - NOTE: 1 yard = 36 inches
-// - Return a string that says “Given XX yard(s), you have YY inches”
-// 3. Run your code.
-// 4. Test your code.
 
 function yards2Inches(yards){
     const yardsToInches = yards * 36;
